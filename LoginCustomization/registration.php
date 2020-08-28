fc6152f9c7f5adacfb66f372b80627849051c691
Asma_AH <Asma.Hawari.AH@gmail.com>
2020-08-29 02:53:54 +0300

    vex solution interview test

---
+++ LoginCustomization/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  * @author Eng.Asma Hawari
+ *  * @package VexSolutions_LoginCustomization
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'VexSolutions_LoginCustomization',
+    __DIR__
+);
